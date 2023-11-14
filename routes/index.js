50140ef23dcb31d0a3725767bb40ce502c7bcf74
priyanshukr449 <priyanshukr449@gmail.com>
2023-11-14 22:48:41 +0530

    ejs setup

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ passport.authenticate(new localStrategy(usersModel.authenticate()));
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-    res.render("index", { title: "Express" });
+    res.render("index");
 });
 
 router.get("/profile", isLoggedIn, function (req, res) {
